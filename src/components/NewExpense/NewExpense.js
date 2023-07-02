cce1d9bfcdbe85f46a3cca4b379c99cd02b4faa7
prajal77 <61044182+prajal77@users.noreply.github.com>
2023-07-02 16:04:12 +0545

    expensesFormAdded

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,9 @@
+import ExpenseForm from './ExpensesForm';
+import './NewExpense.css'
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm/>
+    </div>
+}
+
+export default NewExpense;
