1484161a9c2f9cfcaafae5e93b21fcc21e2832ad
prajal77 <61044182+prajal77@users.noreply.github.com>
2023-07-09 13:12:04 +0545

    ReusingComponent

--- src/components/NewExpense/NewExpense.js
+++
@@ -1,9 +0,0 @@
-import ExpenseForm from './ExpensesForm';
-import './NewExpense.css'
-const NewExpense = () => {
-    return <div className='new-expense'>
-        <ExpenseForm/>
-    </div>
-}
-
-export default NewExpense;
