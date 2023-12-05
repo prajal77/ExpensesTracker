dac795cfbd77e91f0481107f547fda8684b88a71
prajal77 <61044182+prajal77@users.noreply.github.com>
2023-12-05 18:44:44 +0545

    conditional redering using Short Circuting&&

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
 const pizzaData = [
     {
@@ -48,20 +49,81 @@ const pizzaData = [
 
 
 const App = () => {
-    return <div> <h1>Hello React!</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
-
+    return <div className="container">
+        <Header />
+        <Menu />
+        <Footer />
     </div>
 }
 
-const Pizza = () => {
-    return <>
-        <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-        <h1>Pizza Spinaci</h1>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </>
+const Header = () => {
+    // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" }
+    // return <h1 style={style} className="header">Fast React Pizza Co.</h1>
+    return <header className="header"><h1 >Fast React Pizza Co.</h1></header>
+
+
+
+};
+
+const Menu = () => {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+    return <main className="menu">
+        <h2>Our menu</h2>
+        {
+            numPizzas > 0 && (
+                <ul className="pizzas">
+                    {pizzaData.map((pizza) => <Pizza pizzaObj={pizza} key={pizza.name}
+                    />)}
+                </ul>
+            )
+        }
+
+        {/* <Pizza name="Pizza Spinaci" 
+            ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+            img="pizzas/spinaci.jpg"
+            price={12} />
+        <Pizza name="Pizza Spinaci"
+            ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+            img="pizzas/spinaci.jpg"
+            price={12} /> */}
+    </main>
+};
+
+const Pizza = (props) => {
+    return <li className="pizza">
+        <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+        <div>
+            <h3>{props.pizzaObj.name}</h3>
+            <p>{props.pizzaObj.ingredients}</p>
+            <span>{props.pizzaObj.price + 3}</span>
+        </div>
+    </li>
+
+}
+
+const Footer = () => {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+    return <footer className="footer">
+        {/* Conditional Rendering using short circuting( if we have some true or some truthy value such as first
+         value(isOpen) is true the secound value will be retured in case that condition is true   )
+         */}
+        {
+            isOpen && (
+                <div className="order">
+                    <p>We're open until {closeHour}:00. Order Here!!</p>
+                    <button className="btn">Order</button>
+                </div>
+            )
+        }
+        {/* {new Date().toLocaleDateString()}.
+        We're currently open! */}
+    </footer>
+    // return React.createElement("footer", null, "We're currently open!");
 
 }
 
